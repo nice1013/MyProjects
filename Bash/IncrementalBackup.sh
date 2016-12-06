78b805682c7294bbc0e771956bb59bc0bdc493bb
eevanos1@gmail.com <eevanos1@gmail.com>
2016-12-06 05:25:48 +0000

    Completed Incremental Backup foRclone

--- Bash/IncrementalBackup.sh
+++ Bash/IncrementalBackup.sh
@@ -1,38 +1,55 @@
 #!/bin/bash
-#Created by Ed Evanosich December 6th. 
-#This script will do incremental backups to files within a certain folder. 
+#Created by Ed Evanosich December 6th 2016.
+#This script will make a backup using the current date as an extension.
 #We use Rclone to backup to the most popular storage places. 
 
 
 #The first part of our Backup Name 
 partName="Backup"
 #We make backups hourly. For how many days, is based on this var.
-amountofdays=3
+amountofdays=2
 
 
 #We need the current day and the current hour so we can make our extension
 #to our new backup file. 
+#Day of month 1-31
 curday=$(date +"%d")
+#Hour of day 0-23
 curhour=$(date +"%H")
+#Day of the week. 1-7
+dayweek=$(date +"%u")
+#Week of the year. 
+weekyear=$(date +"%W")
 
 #Now our day could be any day in the month. But we only want what our 
 #amountofdays var says. So we simply use that number to get a remainder. 
+#For example if our day is 3. The only possible answers is 0, 1, 2.
 curRemainder=$(($curday % $amountofdays))
 
-#Now we concatenate our current remainder and our curHour and we have our 
-#extension name for this current backup.
-extension=$curRemainder"_"$curhour
+
+#Check to see if this is our weekly backup as that has a different format.
+if [ $dayweek = 1 ] && [ $curhour = 0 ]; then
+    #Weekly format
+    extension=$weekyear"_"$curRemainder"_"$curhour
+else
+    #Regular format
+    extension=$curRemainder"_"$curhour
+fi
+
 
 #Backup file name with extension. 
-filename=$partName"_"$extension
+filename=$partName"_"$extension"/"
+
+
+
 
 #Now we use Rclone to make the backups to our sites.
 #Dropbox
-rclone sync ~/Desktop/TestDropBox "Ed Dropbox": 
+rclone sync ~/Desktop/TestDropBox "Ed Dropbox":$filename
 #Google Drive
-rclone sync ~/Desktop/TestDropBox "Ed Dropbox":
+#rclone sync ~/Desktop/TestDropBox "Ed Dropbox":
 #Amazon
-rclone sync ~/Desktop/TestDropBox "Ed Dropbox":
+#rclone sync ~/Desktop/TestDropBox "Ed Dropbox":
 
 
 
